0aa8a1d0476bb7ed4821cba1e0133681b97e9771
bert13069598 <everglow83@naver.com>
2023-07-28 18:50:52 +0900

    add -D DIR

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#echo "Clone ORB_SLAM3.git"
-#git clone https://github.com/UZ-SLAMLab/ORB_SLAM3.git
-#
-## thirdparty
-#cp ORB_SLAM3/Thirdparty/* thirdparty/ -r
+echo "Clone ORB_SLAM3.git"
+git clone https://github.com/UZ-SLAMLab/ORB_SLAM3.git
+
+# thirdparty
+cp ORB_SLAM3/Thirdparty/* thirdparty/ -r
 
 sudo apt install libboost-dev libboost-serialization-dev -y
 sudo apt install openssl libssl-dev -y
@@ -14,40 +14,43 @@ rm -rf thirdparty/Sophus/build
 rm -rf build
 
 echo "Configuring and building thirdparty/DBoW2 ..."
-cd thirdparty/DBoW2
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cd thirdparty/DBoW2 \
+&&mkdir build \
+&&cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+-DOpenCV_DIR=/home/bert/CLionProjects/orbslam3/thirdparty/opencv/install/Release/lib/cmake/opencv4
 make -j30
 
 cd ../../g2o
 
 echo "Configuring and building thirdparty/g2o ..."
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+mkdir build \
+&&cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+-DEigen3_DIR=/home/bert/CLionProjects/orbslam3/thirdparty/eigen/install/Release/share/eigen3/cmake
 make -j30
 
 cd ../../Sophus
 
 echo "Configuring and building thirdparty/Sophus ..."
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+mkdir build \
+&&cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+-DEigen3_DIR=/home/bert/CLionProjects/orbslam3/thirdparty/eigen/install/Release/share/eigen3/cmake
 make -j30
 
 cd ../../../
 
-#echo "Uncompress vocabulary ..."
-#cd Vocabulary
-#tar -xf ORBvoc.txt.tar.gz
-#cd ..
+echo "Uncompress vocabulary ..."
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
 
-mkdir build
-cd build
+mkdir build \
+&&cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j30
