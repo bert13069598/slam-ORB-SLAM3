599dc28d6c098ff376da60413b1890a7d960c492
bert13069598 <everglow83@naver.com>
2023-08-01 03:18:59 +0900

    DBoW2, fbow

---
+++ examples/LoopClosure/dbow2.cpp
@@ -0,0 +1,208 @@
+#include <boost/filesystem.hpp>
+#include <opencv2/opencv.hpp>
+
+#define USING_EASY_PROFILER
+#include "easy/profiler.h"
+#include "DBoW2.h"
+
+using namespace DBoW2;
+namespace fs = std::filesystem;
+
+using DescriptorVector = std::vector;
+using Descriptors = std::vector;
+using FileLUT = std::vector;
+
+bool loadDBFeatures(DescriptorVector &vfeatures, FileLUT &dbTable, const cv::Ptr &orb);
+
+bool loadVocabulary(const std::unique_ptr &voc);
+
+void changeStructure(const cv::Mat &plain, Descriptors &out);
+
+bool createDatabase(const std::unique_ptr &db, const DescriptorVector &vfeatures);
+
+bool loadQueryFeatures(DescriptorVector &vfeatures, const cv::Ptr &orb);
+
+bool queryDatabase(const std::unique_ptr &db, const DescriptorVector &vfeatures, FileLUT &queryResultTable,
+                   const FileLUT &dbTable);
+
+bool saveResult(const FileLUT &queryResultTable);
+
+
+// Number of Images to Build Database
+constexpr int NDBIMAGES = 4;
+// Number of Images to Query with Database
+constexpr int NQUERYIMAGES = 1;
+
+
+// Paths
+fs::path curPath = fs::current_path();
+fs::path vocPath = curPath.parent_path() / "ORBvoc" / "ORBvoc.txt";
+fs::path dbPath = curPath.parent_path() / "demo" / "Database";
+fs::path queryPath = curPath.parent_path() / "demo" / "Query";
+fs::path savePath = curPath.parent_path() / "demo" / "Result" / "";
+
+
+int main() {
+    EASY_MAIN_THREAD;
+    EASY_PROFILER_ENABLE;
+
+    // DataStructures
+    FileLUT queryResultTable;
+    FileLUT dbTable;
+    DescriptorVector dbFeatures;
+    DescriptorVector queryFeatures;
+    cv::Ptr ORB = cv::ORB::create();
+    auto ptrVocabulary = std::make_unique();
+
+    if (!loadDBFeatures(dbFeatures, dbTable, ORB))
+        std::cerr << "Error while loading DB features\n" << std::endl;
+
+    if (!loadVocabulary(ptrVocabulary))
+        std::cerr << "Error while loading Vocabulary\n" << std::endl;
+
+    EASY_BLOCK("Initialize Database", profiler::colors::LightBlue);
+    auto &vocabulary = *ptrVocabulary;
+    auto ptrDatabase = std::make_unique(vocabulary);
+    EASY_END_BLOCK;
+
+    if (!createDatabase(ptrDatabase, dbFeatures))
+        std::cerr << "Error while Creating DB\n" << std::endl;
+
+    if (!loadQueryFeatures(queryFeatures, ORB))
+        std::cerr << "Error loading Query features\n" << std::endl;
+
+    if (!queryDatabase(ptrDatabase, queryFeatures, queryResultTable, dbTable))
+        std::cerr << "Error while Querying DB\n" << std::endl;
+
+    if (!saveResult(queryResultTable))
+        std::cerr << "Error while saving Results\n" << std::endl;
+
+    profiler::dumpBlocksToFile("DBoW2.prof");
+    return 0;
+}
+
+
+bool loadDBFeatures(DescriptorVector &vfeatures, FileLUT &dbTable, const cv::Ptr &orb) {
+    EASY_FUNCTION("Load DB Features", profiler::colors::LightGreen);
+
+    vfeatures.clear();
+    vfeatures.reserve(NDBIMAGES);
+
+    for (const auto &entry: fs::directory_iterator(dbPath)) {
+        const std::string fileName = entry.path();
+        const cv::Mat image = cv::imread(fileName, 0);
+        if (image.empty())
+            return false;
+
+        cv::Mat mask;
+        std::vector keypoints;
+        cv::Mat descriptors;
+
+        orb->detectAndCompute(image, mask, keypoints, descriptors);
+
+        dbTable.push_back(fileName);
+        vfeatures.emplace_back(Descriptors());
+        changeStructure(descriptors, vfeatures.back());
+    }
+
+    return true;
+}
+
+
+void changeStructure(const cv::Mat &plain, Descriptors &out) {
+    out.resize(plain.rows);
+
+    for (int i = 0; i < plain.rows; ++i)
+        out[i] = plain.row(i);
+}
+
+
+bool loadVocabulary(const std::unique_ptr &voc) {
+    EASY_FUNCTION("Load Vocabulary", profiler::colors::Yellow);
+    if (!voc->loadFromTextFile(vocPath))
+        return false;
+
+    std::cout << "< Vocabulary information >" << std::endl
+              << *voc << std::endl << std::endl;
+
+    return true;
+}
+
+
+bool createDatabase(const std::unique_ptr &db, const DescriptorVector &vfeatures) {
+    EASY_FUNCTION("Create Database", profiler::colors::Magenta);
+
+    for (int i = 0; i < NDBIMAGES; i++)
+        db->add(vfeatures[i]);
+
+    std::cout << "< Database information >\n" << *db << std::endl << std::endl;
+
+    return true;
+}
+
+
+bool loadQueryFeatures(DescriptorVector &vfeatures, const cv::Ptr &orb) {
+    EASY_FUNCTION("Load Query Features", profiler::colors::DeepOrange);
+
+    vfeatures.clear();
+    vfeatures.reserve(NQUERYIMAGES);
+
+    // Feature Extraction for Query Images
+    for (const auto &entry: fs::directory_iterator(queryPath)) {
+        const std::string fileName = entry.path();
+        const cv::Mat image = cv::imread(fileName, 0);
+        if (image.empty())
+            return false;
+
+        cv::Mat mask;
+        std::vector keypoints;
+        cv::Mat descriptors;
+
+        orb->detectAndCompute(image, mask, keypoints, descriptors);
+
+        vfeatures.emplace_back(Descriptors());
+        changeStructure(descriptors, vfeatures.back());
+    }
+
+    return true;
+}
+
+
+bool queryDatabase(const std::unique_ptr &db, const DescriptorVector &vfeatures, FileLUT &queryResultTable,
+                   const FileLUT &dbTable) {
+    EASY_FUNCTION("Query Database", profiler::colors::DarkTeal);
+
+    QueryResults ret;
+    int nCandidates = 1;
+    if (nCandidates > NQUERYIMAGES)
+        return false;
+
+    for (int i = 0; i < NQUERYIMAGES; i++) {
+        // Query & Save "nCandidates" best match in QueryResult
+        db->query(vfeatures[i], ret, nCandidates);
+
+        std::cout << "Searching for Image " << i << ". " << ret << std::endl << std::endl;
+
+        // Save Image index of the Best Match for each Query Image
+        const auto &bestMatch = dbTable[ret[0].Id];
+        queryResultTable.push_back(bestMatch);
+    }
+
+    return true;
+}
+
+
+bool saveResult(const FileLUT &queryResultTable) {
+    // Search Image by index and Save
+    for (int i = 0; i < NQUERYIMAGES; i++) {
+        const cv::Mat image = cv::imread(queryResultTable[i], 0);
+        if (image.empty())
+            return false;
+
+        std::string fileName = savePath;
+        fileName = fileName + "Result" + std::to_string(i) + ".png";
+        cv::imwrite(fileName, image);
+    }
+
+    return true;
+}
---
+++ examples/LoopClosure/fbow.cpp
@@ -0,0 +1,134 @@
+#include <boost/filesystem.hpp>
+#include <opencv2/opencv.hpp>
+
+#define USING_EASY_PROFILER
+#include "easy/profiler.h"
+#include "DBoW2.h"
+#include "dbow2/TemplatedVocabulary.h"
+#include "dbow2/FORB.h"
+#include "fbow.h"
+
+
+using ORBVocabulary = DBoW2::TemplatedVocabulary ;
+using FeatureVector = std::vector>;
+using FileLUT       = std::vector;
+
+namespace fs = std::filesystem;
+
+vector loadFeatures(const std::string& imgPath, int imgNum, const std::string& descriptor);
+
+
+// Paths
+fs::path curPath = fs::current_path();
+fs::path vocPath = curPath.parent_path().parent_path() / "vocabularies" / "ORBvoc.fbow";
+fs::path databasePath = curPath.parent_path().parent_path() / "Database";
+fs::path queryPath = curPath.parent_path().parent_path() / "Query";
+fs::path savePath = curPath.parent_path().parent_path() / "Result" / "";
+
+
+// Number of Database Images
+constexpr int NDBIMAGES = 4;
+// Number of Query Images
+constexpr int NQUERYIMAGES = 1;
+
+
+int main(int argc, char**argv)
+{
+    EASY_MAIN_THREAD;
+    EASY_PROFILER_ENABLE;
+
+    FileLUT dbTable;
+
+    EASY_BLOCK("Loading Vocabulary", profiler::colors::Lime);
+    fbow::Vocabulary voc;
+    voc.readFromFile(vocPath);
+    EASY_END_BLOCK;
+
+
+    EASY_BLOCK("Extract DB Features", profiler::colors::LightBlue);
+    FeatureVector dbFeatures(NDBIMAGES);
+    int idx = 0;
+    for(const auto& entry : fs::directory_iterator(databasePath))
+    {
+        const std::string fileName = entry.path();
+        dbTable.push_back(fileName);
+        dbFeatures[idx++] = loadFeatures(fileName, NDBIMAGES, "orb");
+    }
+    EASY_END_BLOCK;
+
+
+    EASY_BLOCK("Extract QUERY Features", profiler::colors::LightGreen100);
+    FeatureVector queryFeatures(NQUERYIMAGES);
+    idx = 0;
+    for(const auto& entry : fs::directory_iterator(queryPath))
+    {
+        const std::string fileName = entry.path();
+        queryFeatures[idx++] = loadFeatures(fileName, NQUERYIMAGES, "orb");
+    }
+    EASY_END_BLOCK;
+
+
+    // Query Image
+    EASY_BLOCK("Calculate Similarity", profiler::colors::Magenta);
+    fbow::fBow queryBoW, dbBoW;
+    std::map> scores;
+    for (uint64_t i = 0; i < queryFeatures.size(); i++)
+    {
+        queryBoW = voc.transform(queryFeatures[i][0]);
+
+        for (uint64_t j = 0; j < dbFeatures.size(); j++)
+        {
+            dbBoW = voc.transform(dbFeatures[j][0]);
+            double score = queryBoW.score(queryBoW, dbBoW);
+
+            scores.insert(pair(score, j));
+
+            std::cout << score << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    EASY_END_BLOCK;
+
+
+    // Search Image by index and Save
+    EASY_BLOCK("Save Result Image", profiler::colors::Olive);
+    for(int i = 0; i < NQUERYIMAGES; i++)
+    {
+        const std::string readPath = dbTable[scores.begin()->second];
+        const cv::Mat image = cv::imread(readPath, 0);
+
+        const std::string writePath = savePath.string() + "Result" + std::to_string(i) + ".png";
+        cv::imwrite(writePath, image);
+    }
+    EASY_END_BLOCK;
+
+    profiler::dumpBlocksToFile("FBoW.prof");
+
+    return 0;
+}
+
+
+vector loadFeatures(const std::string& imgPath, int imgNum, const std::string& descriptor = "")
+{
+    //select detector
+    cv::Ptr fdetector;
+    assert(descriptor == "orb" || descriptor == "brisk");
+    if (descriptor == "orb")        fdetector = cv::ORB::create(2000);
+
+    else if (descriptor == "brisk") fdetector = cv::BRISK::create();
+
+    else throw std::runtime_error("Invalid descriptor");
+
+    std::vector features;
+    std::vector keypoints;
+    cv::Mat descriptors;
+    const cv::Mat image = cv::imread(imgPath, 0);
+
+    if (image.empty())
+        throw std::runtime_error("Could not open image" + imgPath);
+
+    fdetector->detectAndCompute(image, cv::Mat(), keypoints, descriptors);
+    features.push_back(descriptors);
+
+    return features;
+}
