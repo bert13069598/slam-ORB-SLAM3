3784523e898cbcc1bd9e31aa5a41fea7fffe8e89
bert13069598 <everglow83@naver.com>
2023-08-13 16:37:56 +0900

    run command

---
+++ run.sh
@@ -0,0 +1,11 @@
+# Monocular mode
+./examples/Monocular/mono_kitti \
+./Vocabulary/ORBvoc.txt \
+./examples/Monocular/KITTI00-02.yaml \
+/home/bert/Downloads/datasets/KITTI/data_odometry_color/dataset/sequences/00
+
+# Stereo mode
+./examples/Stereo/stereo_kitti \
+./Vocabulary/ORBvoc.txt \
+./examples/Stereo/KITTI00-02.yaml \
+/home/bert/Downloads/datasets/KITTI/data_odometry_color/dataset/sequences/00
